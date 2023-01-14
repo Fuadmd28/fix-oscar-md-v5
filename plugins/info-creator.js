020ddd08b335e4cd0f9bc95b900cd77450d03730
Fuadmd28 <110433657+Fuadmd28@users.noreply.github.com>
2023-01-14 20:33:30 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -28,7 +28,7 @@ VERSION:3.0
 N:;;;
 FN: ᯤ Bang FD
 item.ORG: ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ
-item1.TEL;waid=628989613496:628989513496@s.whatsapp.net
+item1.TEL;waid=628989513496:628989513496@s.whatsapp.net
 item1.X-ABLabel: ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ
 // Jangan Di Ganti Nomer Yang Bawah!! Jangan Lupa berterima kasih 
 item2.TEL;waid=628989513496:628989513496@s.whatsapp.net
